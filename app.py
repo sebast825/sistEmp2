5abfa0f4a4c6a670a79b805a3e2665d9e75b3031
sebast825 <sebamolina825@gmail.com>
2021-07-23 11:02:41 -0300

    bases

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+from flask import render_template
+from flaskext.mysql import MySQL 
+
+app= Flask(__name__)
+#asdasdasdasdasfsdf
+mysql= MySQL()
+app.config['MYSQL_DATABASE_HOST']='localhost'
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']='root'
+app.config['MYSQL_DATABASE_DB']='empleados'
+mysql.init_app(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
